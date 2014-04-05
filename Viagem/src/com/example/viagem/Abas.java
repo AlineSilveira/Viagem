8ef8faf65f1a73224d7c687fd75b654b090d28ee
AlineSilveira <aline.rmds@gmail.com>
2014-04-05 13:20:24 -0300

    teste

---
+++ Viagem/src/com/example/viagem/Abas.java
@@ -0,0 +1,20 @@
+package com.example.viagem;
+
+import java.util.ArrayList;
+
+public class Abas {
+	
+	String AbaName;
+	ArrayList<String> lista;
+	public Abas(String abaName, String lista[]) {
+		
+		this.AbaName = abaName;
+		this.lista = new ArrayList<String>();
+		for (String string : lista) {
+			this.lista.add(string);
+		}
+	} 
+	
+	
+
+}
